88a60abfbc28d3f47147c890060223b08711cd0c
keishaputritheanny <pkeisha47@gmail.com>
2025-04-28 10:30:55 +0700

    login fixes

--- logic.js
+++ logic.js
@@ -104,17 +104,4 @@ rotatorHolder.addEventListener('mousemove', (e) => {
   rotatorHolder.style.transform = `translateX(${position}px)`;
 });
 
-//Login Page
-document.addEventListener('DOMContentLoaded', () => {
-  const container = document.querySelector('.container2');
-  const registerBtn = document.querySelector('.register-btn');
-  const loginBtn = document.querySelector('.login-btn');
 
-  registerBtn.addEventListener('click', () => {
-    container.classList.add('active');
-  });
-
-  loginBtn.addEventListener('click', () => {
-    container.classList.remove('active');
-  });
-});
